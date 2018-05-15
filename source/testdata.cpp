45923ef8a3730a7e6006b5fe7cb076999ae73beb
Michael Mandl <mandlm@molez.net>
2018-05-15 14:36:53 +0200

    removed metadata

--- source/testdata.cpp
+++ source/testdata.cpp
@@ -1,14 +1,5 @@
 #include "testdata.h"
 
-void TestData::Meta::write(QJsonObject &json)
-{
-	json["participant name"] = m_participant;
-	json["instructor name"] = m_instructor;
-	json["date of birth"] = m_dateOfBirth.toString(Qt::TextDate);
-	json["date of test"] = m_dateOfTest.toString(Qt::TextDate);
-	json["remarks"] = m_remarks;
-}
-
 void TestData::SubTest1::write(QJsonObject &json)
 {
 	json["type"] = "SubTest1";
@@ -16,10 +7,6 @@ void TestData::SubTest1::write(QJsonObject &json)
 
 void TestData::write(QJsonObject &json)
 {
-	QJsonObject metaData;
-	m_metaData.write(metaData);
-	json["Meta"] = metaData;
-
 	QJsonObject subTest1Data;
 	m_subTest1Data.write(subTest1Data);
 	json["SubTest1"] = subTest1Data;
--- source/testdata.h
+++ source/testdata.h
@@ -7,19 +7,6 @@
 class TestData
 {
 private:
-	class Meta
-	{
-	private:
-		QString m_participant;
-		QString m_instructor;
-		QDate m_dateOfBirth;
-		QDate m_dateOfTest;
-		QString m_remarks;
-
-	public:
-		void write(QJsonObject &json);
-	};
-
 	class SubTest1
 	{
 	public:
@@ -27,7 +14,6 @@ private:
 	};
 
 private:
-	Meta m_metaData;
 	SubTest1 m_subTest1Data;
 
 public:
