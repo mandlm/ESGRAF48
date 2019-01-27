c044bebece17b7d9b64b027931365c2fd8e4568c
Michael Mandl <mandlm@molez.net>
2019-01-27 20:09:10 +0100

    Use the maximum test-age for subjects past that age

--- source/MetaData/MetaDataModel.h
+++ source/MetaData/MetaDataModel.h
@@ -24,19 +24,14 @@ public:
 	MetaDataModel(QObject *parent);
 	int rowCount(const QModelIndex &parent = QModelIndex()) const override;
 	int columnCount(const QModelIndex &parent = QModelIndex()) const override;
-	QVariant data(
-		const QModelIndex &index, int role = Qt::DisplayRole) const override;
+	QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
 	Qt::ItemFlags flags(const QModelIndex &index) const override;
-	bool setData(const QModelIndex &index, const QVariant &value,
-		int role = Qt::EditRole) override;
+	bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
 
 	void read(const ESGRAF48::MetaDataModel &model);
 	void write(ESGRAF48::MetaDataModel &model) const;
 
 	std::string toHtml() const;
 
-	Age getAge() const
-	{
-		return { m_dateOfBirth, m_dateOfTest };
-	}
+	Age getAge() const;
 };
